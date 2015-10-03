879dd760854129528a73011be831ef1617662ea8
Mark Fischer, Jr. <flyingfisch@toppagedesign.com>
2015-10-03 13:58:55 -0400

    added a comment to the twisted example

--- hellotwisted.py
+++ hellotwisted.py
@@ -8,6 +8,7 @@ class Hello(resource.Resource):
         request.setHeader('content-type', 'text/plain')
         return 'Hello World!'
 
+# Check http://localhost:8080 to see the result!
 endpoints.serverFromString(reactor, 'tcp:8080').listen(server.Site(Hello()))
 reactor.run()
 
